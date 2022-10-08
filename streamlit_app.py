817bd2e7d012b70a255120ee3091e818c7a6513c
AM1ps <115346928+AM1ps@users.noreply.github.com>
2022-10-08 16:13:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit
 streamlit.title("My Parents Healthy Dinner");
 streamlit.header('Breakfast Menu');
 streamlit.text("🥣Omega3 & Blueberry Oatmeal");
-streamlit.text("🥗Kale,Spinach Rocket Smoothie");
-streamlit.text("🐔Hard-Boiled Free-Range Egg");
-streamlit.text('🥑🍞Avocado Toast');
+streamlit.text("🥗Kale,Spinach,Rocket Smoothie");
+streamlit.text("🐔Hard-Boiled , Free-Range Egg");
+streamlit.text('🥑🍞Avocado ,Toast');
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
